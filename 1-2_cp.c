726763b69450de0844abe56af78418b5b2ca53c1
zhibli <zhibli@redhat.com>
2018-02-09 15:45:35 +0800

    simple cp implementation
    note:while(n = read() > 0) is wrong because at this moment n can only be 0 or 1 after the comparison between the return value of read() and 0.

--- 1-2_cp.c
+++ 1-2_cp.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <fcntl.h>
 
-int main(void)
+int main(int argc, char **argv)
 {
 	int n;
-	char buf[BUFFSIZE];
-	while(n = read(STDIN_FILENO, buf, BUFFSIZE) > 0){
-		if(write(STDOUT_FILENO, buf, n) != n){
+	char buf[10];
+	int fd1 = open(argv[1], O_RDONLY);
+	int fd2 = open(argv[2], O_RDWR);
+	//10 is the buffer size defined
+	//注意while(n = read(fd1, buf, 10) > 0)是错的，这样会先比较是否大于0，true则返回1，这时赋给n的值为1
+	while((n = read(fd1, buf, 10)) > 0){
+		if(write(fd2, buf, n) != n){
 			printf("error\n");
 			exit(1);
 		}
+	}
 	exit(0);
 }
