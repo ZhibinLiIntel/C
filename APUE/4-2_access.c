6653fa41fb07fa744cf388fd6fdfd942f131f0ff
Zhibin Li <zhibli@redhat.com>
2018-04-03 17:00:35 +0800

    add color support and verify "setuid" function

--- APUE/4-2_access.c
+++ APUE/4-2_access.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
 
 int main(int argc, char **argv)
 {
@@ -8,9 +11,20 @@ int main(int argc, char **argv)
 		printf("Usage: a.out <file>\n");
 		exit(1);
 	}
-	if (access(argv[1], F_OK) == 0)
-		printf("file exists\n");
+	if (access(argv[1], F_OK) == 0) {
+		if (access(argv[1], R_OK) == 0)
+			printf("Read \033[1;32m[OK]\n\033[0m");
+		else {
+			printf("Read \033[1;31m[FAILED]\033[0m");
+			printf(" - %s\n", strerror(errno));
+		}
+		if (open(argv[1], O_RDONLY) > 0)
+			printf("Open \033[1;32m[OK]\n\033[0m");
+		else {
+			printf("Open \033[1;31m[FAILED]\033[0m");
+			printf(" - %s\n", strerror(errno));
+		}
+	}
 	else
-		printf("file does not exist\n");
-	return 0;
+		printf("File does not exist\n");
 }
