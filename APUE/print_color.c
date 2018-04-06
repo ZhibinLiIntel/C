45c95ec0beb1d60da38d80cd127a1190865d4ad4
Zhibin Li <08826794brmt@gmail.com>
2018-04-07 00:02:26 +0800

    bold color support

--- APUE/print_color.c
+++ APUE/print_color.c
@@ -1,21 +1,33 @@
 #include <stdio.h>
 
 #define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_BOLD_RED     "\x1b[1;31m"
 #define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_BOLD_GREEN   "\x1b[1;32m"
 #define ANSI_COLOR_YELLOW  "\x1b[33m"
+#define ANSI_COLOR_BOLD_YELLOW  "\x1b[1;33m"
 #define ANSI_COLOR_BLUE    "\x1b[34m"
+#define ANSI_COLOR_BOLD_BLUE    "\x1b[1;34m"
 #define ANSI_COLOR_MAGENTA "\x1b[35m"
+#define ANSI_COLOR_BOLD_MAGENTA "\x1b[1;35m"
 #define ANSI_COLOR_CYAN    "\x1b[36m"
+#define ANSI_COLOR_BOLD_CYAN    "\x1b[1;36m"
 #define ANSI_COLOR_RESET   "\x1b[0m"
 
 int main (int argc, char const *argv[]) {
 
   printf(ANSI_COLOR_RED     "This text is RED!"     ANSI_COLOR_RESET "\n");
+  printf(ANSI_COLOR_BOLD_RED     "This text is bold RED!"     ANSI_COLOR_RESET "\n");
   printf(ANSI_COLOR_GREEN   "This text is GREEN!"   ANSI_COLOR_RESET "\n");
+  printf(ANSI_COLOR_BOLD_GREEN   "This text is bold GREEN!"   ANSI_COLOR_RESET "\n");
   printf(ANSI_COLOR_YELLOW  "This text is YELLOW!"  ANSI_COLOR_RESET "\n");
+  printf(ANSI_COLOR_BOLD_YELLOW  "This text is bold YELLOW!"  ANSI_COLOR_RESET "\n");
   printf(ANSI_COLOR_BLUE    "This text is BLUE!"    ANSI_COLOR_RESET "\n");
+  printf(ANSI_COLOR_BOLD_BLUE    "This text is bold BLUE!"    ANSI_COLOR_RESET "\n");
   printf(ANSI_COLOR_MAGENTA "This text is MAGENTA!" ANSI_COLOR_RESET "\n");
+  printf(ANSI_COLOR_BOLD_MAGENTA "This text is bold MAGENTA!" ANSI_COLOR_RESET "\n");
   printf(ANSI_COLOR_CYAN    "This text is CYAN!"    ANSI_COLOR_RESET "\n");
+  printf(ANSI_COLOR_BOLD_CYAN    "This text is bold CYAN!"    ANSI_COLOR_RESET "\n");
 
   return 0;
 }
