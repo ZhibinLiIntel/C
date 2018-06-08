77a1a78bd44d9dae54535d50973761e7be9667d2
haoyouab <08826794brmt@gmail.com>
2018-06-08 02:04:30 +0000

    APUE/3-2_lseek-with-blank.c: add comment about block size

--- APUE/3-2_lseek-with-blank.c
+++ APUE/3-2_lseek-with-blank.c
@@ -6,13 +6,15 @@
 
 int main(void)
 {
+	/* Note: Linux's block size is now 4kB(4096 bytes),
+	not 512 bytes mentioned in APUE book. */
 	char a[] = "ABCDEFGH";
 	char b[] = "abcdefgh";
 	int fd;
 	fd = open("file_with_blank", O_RDWR|O_CREAT|O_EXCL);
-	write(fd, a, strlen(a));
+	write(fd, a, sizeof(a));
 	lseek(fd, 1234, SEEK_SET);
-	write(fd, b, strlen(b));
+	write(fd, b, sizeof(b));
 	close(fd);
 	return 0;
 }
